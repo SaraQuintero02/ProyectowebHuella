78396c403fc39054d28eae346ab67965d9c50a23
SaraQuintero02 <squintero@uniempresarial.edu.co>
2025-09-22 20:28:55 -0500

    terminandooo

--- rescue/src/app/app.routes.ts
+++ rescue/src/app/app.routes.ts
@@ -5,7 +5,6 @@ import { ChatbotComponent } from './chat-bot/chat-bot';
 import { DonarComponent } from './donar/donar';
 import { MigapanComponent } from './migapan/migapan';
 import { MisionVisionComponent } from './mision-vision/mision-vision';
-import { Estadisticas } from './estadisticas/estadisticas';
 import { PerfilesComponent } from './perfiles/perfiles';
 import { PoliticaPrivacidad } from './politica-privacidad/politica-privacidad';
 import { ProcesoAdopcionComponent } from './proceso-adopcion/proceso-adopcion';
@@ -14,6 +13,8 @@ import { ReportesComponent } from './reportes/reportes';
 import { VoluntarioComponent } from './voluntario/voluntario';
 import { Contacto } from './contacto/contacto';
 import { Login } from './login/login';
+import { Estadisticas } from './estadisticas/estadisticas';
+
 
 export const routes: Routes = [
   { path: 'inicio', component: InicioComponent },
@@ -23,7 +24,6 @@ export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'migapan', component: MigapanComponent },
   { path: 'mision-vision', component: MisionVisionComponent },
-  { path: 'estadisticas', component: Estadisticas },
   { path: 'perfiles', component: PerfilesComponent },
   { path: 'proceso-adopcion', component: ProcesoAdopcionComponent },
   { path: 'registro', component: RegistroComponent },
@@ -31,6 +31,7 @@ export const routes: Routes = [
   { path: 'voluntarios', component: VoluntarioComponent },
   { path: 'politica-privacidad', component: PoliticaPrivacidad },
   { path: 'contacto', component: Contacto },
+    { path: 'estadisticas', component: Estadisticas},
   { path: '', redirectTo: 'inicio', pathMatch: 'full' }
 ];
 
--- rescue/src/app/estadisticas/estadisticas.ts
+++ rescue/src/app/estadisticas/estadisticas.ts
@@ -1,12 +1,55 @@
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';//Peticiones http, usa httpclient:Servicio
+
+//Estructura de los datos
+interface Registro {
+  a_o?: string;
+  fecha_de_realizacion?: string;
+  municipio?: string;
+  caninos_hembras?: number;
+  caninos_machos?: number;
+  felinos_machos?: number;
+  felinos_hembras?: number;
+  total_n_de_animales?: number;
+  decesos?: number;
+}
 
 @Component({
   selector: 'app-estadisticas',
-  imports: [CommonModule],
+  standalone: true,
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './estadisticas.html',
-  styleUrl: './estadisticas.css'
+  styleUrls: ['./estadisticas.css']
 })
-export class Estadisticas {
+export class Estadisticas implements OnInit {
 
+  estadisticas: Registro[] = [];
+  private BASE_URL = "https://www.datos.gov.co/resource/wm9s-qtjx.json";
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.cargarDatos();
+  }
+
+  cargarDatos(): void {
+    this.http.get<Registro[]>(this.BASE_URL).subscribe({
+      next: (datos) => this.estadisticas = datos,
+      error: (err) => console.error('Error al consumir la API:', err)
+    });
+  }
+
+  //Funciones de accesibilidad
+  aumentarFuente() {
+    document.body.style.fontSize = 'larger';
+  }
+
+  disminuirFuente() {
+    document.body.style.fontSize = 'smaller';
+  }
+
+  cambiarContraste() {
+    document.body.classList.toggle('contraste-activo');
+  }
 }
